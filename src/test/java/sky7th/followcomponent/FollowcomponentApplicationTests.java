866fec8261133d21e850127be5bdef8ef108e7bc
이태화 <xoghk0321@gmail.com>
2019-11-21 16:13:12 +0900

    init

---
+++ src/test/java/sky7th/followcomponent/FollowcomponentApplicationTests.java
@@ -0,0 +1,13 @@
+package sky7th.followcomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowcomponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
