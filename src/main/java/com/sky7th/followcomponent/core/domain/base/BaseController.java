11fa119f19d0be0750057d1820457412a2fb8ca9
이태화 <xoghk0321@gmail.com>
2019-11-21 17:22:35 +0900

    기능: Controller 에서 응답 모델 만들기 쉽게 BaseController 추가

---
+++ src/main/java/com/sky7th/followcomponent/core/domain/base/BaseController.java
@@ -0,0 +1,54 @@
+package com.sky7th.followcomponent.core.domain.base;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class BaseController {
+
+	protected static final String APPLICATION_JSON = "application/json; charset=UTF-8";
+
+	protected HashMap<String, Object> getSuccessResult() {
+		HashMap<String, Object> retMap = new HashMap<>();
+		retMap.put("result", "success");
+		return retMap;
+	}
+
+	protected HashMap<String, Object> getSuccessResult(String msg) {
+		HashMap<String, Object> retMap = getSuccessResult();
+		retMap.put("msg", msg);
+		return retMap;
+	}
+
+	protected HashMap<String, Object> getSuccessResult(Object data) {
+		HashMap<String, Object> retMap = getSuccessResult();
+		retMap.put("data", data);
+		return retMap;
+	}
+
+	protected HashMap<String, Object> getSuccessResult(Object data, String msg) {
+		HashMap<String, Object> retMap = getSuccessResult(data);
+		retMap.put("msg", msg);
+		return retMap;
+	}
+
+	protected HashMap<String, Object> getFailResult() {
+		HashMap<String, Object> retMap = new HashMap<>();
+		retMap.put("result", "failed");
+		return retMap;
+	}
+
+	protected HashMap<String, Object> getFailResult(String errMsg) {
+		HashMap<String, Object> retMap = getFailResult();
+		retMap.put("errmsg", errMsg);
+		return retMap;
+	}
+
+	protected HashMap<String, Object> getFailResult(String errMsg, Object data) {
+		HashMap<String, Object> retMap = getFailResult(errMsg);
+		retMap.put("data", data);
+		return retMap;
+	}
+
+}
